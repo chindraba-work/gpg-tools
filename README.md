6da3ab6cf3d0f2dcb5799447f37ed2fb3d4fc1f2
Ronald Lamoreaux <gitkey@chindraba.work>
2021-05-18 01:49:16 +0000

    Initial commit to establish the repo
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

---
+++ README.md
@@ -0,0 +1,39 @@
+# Chindraba Website
+
+The website for Chindraba as a developer.
+
+Subject to constant change to update methods and trials of new ideas.
+
+Intentions are that the site will be dynamic and responsive, but not based upon
+and "framework" or other technologies than HTML5, CSS3 and JavaScript, generated
+using PHP and/or Perl.
+
+## Copyright and License
+
+The MIT license applies to all the code within this repository.
+
+Copyright © 2021 Chindraba (Ronald Lamoreaux) <main-site@chindraba.work>
+
+    All Rights Reserved
+
+## The MIT License
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGE-
+    MENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+    FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ htdocs/index.php
@@ -0,0 +1,47 @@
+#! /usr/bin/env php
+<?php
+// SPDX-License-Identifier: MIT
+
+/***********************************************************************
+*                                                                      *
+*  This file is part of collection of scripts and tools created for my *
+*  personal needs. Usefulness to others might exists                   *
+*                                                                      *
+*  Copyright © 2021  Chindraba (Ronald Lamoreaux)                      *
+*                    <main-site@chindraba.work>                        *
+*  - All Rights Reserved                                               *
+*                                                                      *
+*  Permission is hereby granted, free of charge, to any person         *
+*  obtaining a copy of this software and associated documentation      *
+*  files (the "Software"), to deal in the Software without             *
+*  restriction, including without limitation the rights to use, copy,  *
+*  modify, merge, publish, distribute, sublicense, and/or sell copies  *
+*  of the Software, and to permit persons to whom the Software is      *
+*  furnished to do so, subject to the following conditions:            *
+*                                                                      *
+*  The above copyright notice and this permission notice shall be      *
+*  included in all copies or substantial portions of the Software.     *
+*                                                                      *
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,     *
+*  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  *
+*  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND               *
+*  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS *
+*  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN  *
+*  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN   *
+*  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    *
+*  SOFTWARE.                                                           *
+*                                                                      *
+************************************************************************/
+?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us" lang="en-us">
+<head>
+    <title>Just Beginning</title>
+</head>
+<body>
+    <h1>Just Beginning</h1>
+</body>
+</html>
+
+<?php
+// vim: set syntax=php ts=4 sw=4 sts=4 et sr:
